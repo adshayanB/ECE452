88613a98d7b4dd5a2ba06b56c0258d7fd49c2bc1
Preyansh Kachhia <pkachhia@uwaterloo.ca>
2023-06-25 16:08:24 -0400

    fix: changed position to bottom right

--- app/src/main/java/com/example/farmeraid/uicomponents/FloatingActionButtonView.kt
+++ app/src/main/java/com/example/farmeraid/uicomponents/FloatingActionButtonView.kt
@@ -1,7 +1,10 @@
 package com.example.farmeraid.uicomponents
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Add
@@ -10,6 +13,7 @@ import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -23,23 +27,27 @@ fun FloatingActionButtonView(
     fabUiEvent: UiComponentModel.FabUiEvent,
     modifier: Modifier = Modifier,
 ) {
-    FloatingActionButton(
-        modifier = modifier,
-        onClick = fabUiEvent.onClick,
-        containerColor = PrimaryColour,
-    ) {
-        if (fabUiState.icon.compareTo("Add") == 0) {
-            Icon(
-                imageVector = Icons.Rounded.Add,
-                contentDescription = "Add New Item",
-                tint = WhiteContentColour,
-            )
-        } else {
-            Icon(
-                imageVector = Icons.Rounded.Mic,
-                contentDescription = "Mic",
-                tint = WhiteContentColour,
-            )
+    Box(modifier = Modifier.fillMaxSize()) {
+        FloatingActionButton(
+            modifier = modifier
+                .padding(all = 20.dp)
+                .align(alignment = Alignment.BottomEnd),
+            onClick = fabUiEvent.onClick,
+            containerColor = PrimaryColour,
+        ) {
+            if (fabUiState.icon.compareTo("Add") == 0) {
+                Icon(
+                    imageVector = Icons.Rounded.Add,
+                    contentDescription = "Add New Item",
+                    tint = WhiteContentColour,
+                )
+            } else {
+                Icon(
+                    imageVector = Icons.Rounded.Mic,
+                    contentDescription = "Mic",
+                    tint = WhiteContentColour,
+                )
+            }
         }
     }
 }
