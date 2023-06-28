83ca53a01a50f4a200351e69f9a29c74dc55d406
Rishan Ratnasivam <r2ratnas@uwaterloo.ca>
2023-06-27 20:39:13 -0400

    Create quotas page

--- app/src/main/java/com/example/farmeraid/data/QuotasRepository.kt
+++ app/src/main/java/com/example/farmeraid/data/QuotasRepository.kt
@@ -7,15 +7,74 @@ import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 
 class QuotasRepository {
-    fun getCategorizedQuotas(): Flow<List<HomeModel.CategorizedQuotas>> {
+    fun getCategorizedQuotas(): Flow<List<HomeModel.Quota>> {
         return flow {
-            val quotasList = listOf(HomeModel.CategorizedQuotas(
-                marketName = "test",
-                quotas = listOf(HomeModel.Quota(
-                    produceName = "Test Produce",
-                    produceGoalAmount = 25,
-                    produceSoldAmount = 10,
-                ))
+            val quotasList = listOf(HomeModel.Quota(
+                marketName = "St. Jacob's",
+                produceQuotaList = listOf(
+                    HomeModel.ProduceQuota(
+                        produceName = "Apple",
+                        produceGoalAmount = 25,
+                        produceSoldAmount = 10,
+                    ),
+                    HomeModel.ProduceQuota(
+                        produceName = "Bananas",
+                        produceGoalAmount = 10,
+                        produceSoldAmount = 9,
+                    ),
+                )
+            ), HomeModel.Quota(
+                marketName = "St. Lawrence",
+                produceQuotaList = listOf(
+                    HomeModel.ProduceQuota(
+                        produceName = "Carrots",
+                        produceGoalAmount = 15,
+                        produceSoldAmount = 10,
+                    ),
+                    HomeModel.ProduceQuota(
+                        produceName = "Oranges",
+                        produceGoalAmount = 24,
+                        produceSoldAmount = 7,
+                    ),
+                )
+            ), HomeModel.Quota(
+                marketName = "Another one",
+                produceQuotaList = listOf(
+                    HomeModel.ProduceQuota(
+                        produceName = "Apples",
+                        produceGoalAmount = 4,
+                        produceSoldAmount = 3,
+                    ),
+                    HomeModel.ProduceQuota(
+                        produceName = "Bananas",
+                        produceGoalAmount = 10,
+                        produceSoldAmount = 1,
+                    ),
+                    HomeModel.ProduceQuota(
+                        produceName = "Pears",
+                        produceGoalAmount = 5,
+                        produceSoldAmount = 0,
+                    ),
+                )
+            ), HomeModel.Quota(
+                marketName = "Another one",
+                produceQuotaList = listOf(
+                    HomeModel.ProduceQuota(
+                        produceName = "Apples",
+                        produceGoalAmount = 4,
+                        produceSoldAmount = 3,
+                    ),
+                    HomeModel.ProduceQuota(
+                        produceName = "Bananas",
+                        produceGoalAmount = 10,
+                        produceSoldAmount = 1,
+                    ),
+                    HomeModel.ProduceQuota(
+                        produceName = "Pears",
+                        produceGoalAmount = 5,
+                        produceSoldAmount = 0,
+                    ),
+                )
             ))
             emit(quotasList)
         }.flowOn(Dispatchers.IO)
--- app/src/main/java/com/example/farmeraid/home/views/HomeScreenView.kt
+++ app/src/main/java/com/example/farmeraid/home/views/HomeScreenView.kt
@@ -1,15 +1,34 @@
 import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.TabRowDefaults
+import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.farmeraid.home.HomeViewModel
 import com.example.farmeraid.home.model.HomeModel.Tab
 import com.example.farmeraid.navigation.NavRoute
+import com.example.farmeraid.ui.theme.PrimaryColour
 import com.example.farmeraid.uicomponents.ButtonView
+import com.example.farmeraid.uicomponents.FloatingActionButtonView
 import com.example.farmeraid.uicomponents.OutlinedButtonView
 import com.example.farmeraid.uicomponents.models.UiComponentModel
 
@@ -21,15 +40,66 @@ fun HomeScreenView() {
     val state by viewModel.state.collectAsState()
 
     Scaffold(
-       floatingActionButton = {
-           ButtonView(
-               buttonUiState = state.buttonUiState,
-               buttonUiEvent = UiComponentModel.ButtonUiEvent(
-                   onClick = { viewModel.changeSelectedTabAndNavigate(if (state.selectedTab == Tab.Quotas) Tab.Inventory else Tab.Quotas) }
-               )
-           )
-       },
-    ) {
-        Text(state.selectedTab.toString())
+        floatingActionButton = {
+            FloatingActionButtonView(
+                fabUiState = UiComponentModel.FabUiState(
+                    icon = Icons.Filled.Add,
+                    contentDescription = "Add Quota",
+                ),
+                fabUiEvent = UiComponentModel.FabUiEvent(
+                    onClick = { viewModel.navigateToAddQuota() }
+                )
+            )
+        },
+        topBar = {
+            TabRow(
+                selectedTabIndex = state.selectedTab.index,
+                contentColor = PrimaryColour,
+                indicator = { tabPositions -> TabRowDefaults.Indicator(
+                    modifier = Modifier.tabIndicatorOffset(tabPositions[state.selectedTab.index]),
+                    color = PrimaryColour,
+                ) },
+            ) {
+                Tab(
+                    selected = state.selectedTab == Tab.Quotas,
+                    onClick = { viewModel.changeSelectedTab(Tab.Quotas) },
+                    text = {
+                        Text(
+                            text = Tab.Quotas.name,
+                            maxLines = 2,
+                            overflow = TextOverflow.Ellipsis
+                        )
+                    }
+                )
+                Tab(
+                    selected = state.selectedTab == Tab.Inventory,
+                    onClick = { viewModel.changeSelectedTab(Tab.Inventory) },
+                    text = {
+                        Text(
+                            text = Tab.Inventory.name,
+                            maxLines = 2,
+                            overflow = TextOverflow.Ellipsis
+                        )
+                    }
+                )
+            }
+        }
+    ) { paddingValues ->
+        LazyColumn (
+            modifier = Modifier
+                .padding(paddingValues)
+                .padding(20.dp),
+            verticalArrangement = Arrangement.spacedBy(30.dp),
+        ) {
+            if (state.selectedTab == Tab.Quotas) {
+                items(state.quotasList) { quota ->
+                    QuotaItem(quota = quota)
+                }
+            } else {
+                items(state.inventoryList) {
+
+                }
+            }
+        }
     }
 }
