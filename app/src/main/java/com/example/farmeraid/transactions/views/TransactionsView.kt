6fcfb69f83b3933e435a5d104af517accc48c73e
Soumil Dharaskar <sdharask@uwaterloo.ca>
2023-07-05 22:36:27 -0400

    Added lazy row and reformatted column on transactions page

--- app/src/main/java/com/example/farmeraid/transactions/views/TransactionsView.kt
+++ app/src/main/java/com/example/farmeraid/transactions/views/TransactionsView.kt
@@ -1,36 +1,58 @@
 import android.annotation.SuppressLint
+import android.support.customtabs.ICustomTabsCallback
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.outlined.Close
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.rememberNavController
 import com.example.farmeraid.transactions.TransactionsViewModel
 import com.example.farmeraid.ui.theme.PrimaryColour
 import com.example.farmeraid.ui.theme.WhiteContentColour
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
@@ -58,51 +80,61 @@ fun TransactionsView() {
             )
         },
     ) {paddingValues ->
-        LazyColumn(
+        Column(
             modifier = Modifier
-                .padding(paddingValues),
-            verticalArrangement = Arrangement.spacedBy(20.dp),
-            contentPadding = PaddingValues(20.dp),
+                .padding(paddingValues)
+                .padding(20.dp, 20.dp, 20.dp, 0.dp),
         ){
-            items(state.transactionList) { trans ->
-                Card(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .clickable { },
-                    elevation = CardDefaults.cardElevation(
-                        defaultElevation = 10.dp
-                    )
-                ){
-                    Row(modifier = Modifier
-                        .fillMaxWidth(),
-                        horizontalArrangement = Arrangement.SpaceBetween
-                    ){
-                        Text(modifier = Modifier
-                            .padding(10.dp),
-                            text = trans.transactionType,
-                            color = Color.Black,
-                            fontSize = 25.sp
+            LazyRow {
+                //filter pills
+            }
+            Spacer(modifier = Modifier.height(20.dp))
+            Divider()
+            LazyColumn(
+                verticalArrangement = Arrangement.spacedBy(20.dp),
+                contentPadding = PaddingValues(0.dp, 20.dp),
+            ){
+                items(state.transactionList) { trans ->
+                    Card(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .clickable { },
+                        elevation = CardDefaults.cardElevation(
+                            defaultElevation = 10.dp
                         )
-                        IconButton(
-                            modifier = Modifier,
-                            onClick = { viewModel.showDeleteConfirmation(trans.transactionId) })
-                        {
-                            Icon(Icons.Outlined.Close, contentDescription = "Localized description")
+                    ){
+                        Row(modifier = Modifier
+                            .fillMaxWidth(),
+                            horizontalArrangement = Arrangement.SpaceBetween
+                        ){
+                            Text(modifier = Modifier
+                                .padding(10.dp),
+                                text = trans.transactionType,
+                                color = Color.Black,
+                                fontSize = 25.sp
+                            )
+                            IconButton(
+                                modifier = Modifier,
+                                onClick = { viewModel.showDeleteConfirmation(trans.transactionId) })
+                            {
+                                Icon(Icons.Outlined.Close, contentDescription = "Localized description")
+                            }
                         }
-                    }
-                    Row(
+                        Row(
 
-                    ){
-                        Text(modifier = Modifier
-                            .padding(10.dp, 0.dp, 0.dp, 10.dp),
-                            text = trans.transactionMessage, color = Color.Black,
-                            fontSize = 18.sp
-                        )
-                    }
+                        ){
+                            Text(modifier = Modifier
+                                .padding(10.dp, 0.dp, 0.dp, 10.dp),
+                                text = trans.transactionMessage, color = Color.Black,
+                                fontSize = 18.sp
+                            )
+                        }
 
+                    }
                 }
             }
         }
+
     }
 }
 
